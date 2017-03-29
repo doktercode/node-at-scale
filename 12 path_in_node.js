d357fc9ea46ffcf294f8c78d0c01d6fcd74b303d
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-29 17:45:12 +0530

    path in node.js

---
+++ 12 path_in_node.js
@@ -0,0 +1,23 @@
+/**
+ * Path module in Node.js:
+ * ----------------------
+ * 
+ *      Node.js provides path module to normalize, join, and resolve file system paths.
+ *      Find relative paths, extract components from paths and determine existence of paths.
+ *      The path module simply manipulates strings and does not interact with the file system to validate the paths.
+ * 
+ *      Functions provided by path module in node.js:
+ *      ---------------------------------------------
+ *      
+ *       path.normalize() - Ensure a predictable format when a source is untrusted or unreliable.
+ *       path.join() - Build a single path from path segments.
+ *       path.dirname() - Provides a directory name out of the path.
+ *       path.basename() - Provide the final path segment.
+ *       path.extname() - Use to slice from the last period(.) to the end of the path string.
+ *       path.resolve() - Resolve a list of path instructions into an absolute path.
+ *       
+ *       
+ */ 
+
+
+
